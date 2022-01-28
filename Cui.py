3667ba853bbfbc12f72fdc5794219157e4fd6c02
DezhouCuixiaobing <384532015@qq.com>
2022-01-28 10:58:26 +0800

    增加读取文件内容的功能

--- Cui.py
+++ Cui.py
@@ -21,12 +21,55 @@ class Folder:
             else:
                 return Folder.Dealing(file)
 
+#描述符
+class DataFrame:
+    __count = 0
+    def __init__(self):
+        cls = self.__class__
+        prefix = cls.__name__
+        index = cls.__count
+        self.storage_name =  '{}_{}'.format(prefix,index)
+        __count += 1
+
+    def __get__(self,instance,owner):
+        return getattr(instance,self.storage_name)
+
+    def __set__(self,instance,value):
+        setattr(instance, self.storage_name, value)
+
+#变量名生成器
+
+
+
+
+#输入字典，生成元素为DataFrame的变量名的列表
 class Files:
     def __init__(self,dic):
         self.dic = dic
+        self.method = {
+            "txt":"pd.read_table",
+            "xls":"pd.read_excel",
+            "xlxs":"pd.read_excel",
+            "csv":"pd.read_csv"
+        }
+        self.list = []
 
     def __repr__(self):
-        for path,key in self.dic.items():
+        #用数字作为变量名！！！！
+        for path,key,name in self.dic.items(),range(len(self.dic)):
+            if key == 'txt':
+                name = DataFrame()
+                name = eval(self.method[key])(path,sep = ',')
+            else:
+                name = DataFrame()
+                name = eval(self.method[key])(path)
+            self.list.append(name)
+        return self.list
+
+
+
+
+
 
 
 
